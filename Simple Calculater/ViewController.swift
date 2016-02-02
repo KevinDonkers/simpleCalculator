9546edb89abc7e8a82967ca6b5199b16b468c824
student <student@students-mac.student.georgianc.on.ca>
2016-02-02 14:13:22 -0500

    added comments

--- Simple Calculater/ViewController.swift
+++ Simple Calculater/ViewController.swift
@@ -2,20 +2,25 @@
 //  ViewController.swift
 //  Simple Calculater
 //
-//  Created by student on 2016-02-02.
+//  Created by Kevin Donkers on 2016-02-02.
 //  Copyright © 2016 student. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    //Outlets
     @IBOutlet weak var displayLabel: UILabel!
     
+    //variables
     var num1 = 0.0, num2 = 0.0
     var operation = ""
 
+    //Click event handlers for the number keys
     @IBAction func oneClicked(sender: UIButton) {
+        //check if I reached the digit limit of the calculator
         if displayLabel.text?.characters.count < 6 {
+            //if i am within the limit add a 1 to the display
             displayLabel.text = displayLabel.text! + "1"
         }
     }
@@ -74,20 +79,29 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    
+    
+    //add a decimal point to the display if there is room
     @IBAction func decimalClicked(sender: UIButton) {
         if displayLabel.text?.characters.count < 6 {
             displayLabel.text = displayLabel.text! + "."
         }
     }
     
+    //clear the display
     @IBAction func clearClicked(sender: UIButton) {
         displayLabel.text = ""
     }
-
+    
+    
+    //action handlers for the operations buttons
     @IBAction func AddClicked(sender: UIButton) {
+        //set whatever number is on the screen when they click add to the first number of the equation nd clear the screen after
         num1 = (displayLabel.text! as NSString).doubleValue
         displayLabel.text = ""
         
+        //set what operation im preforming for the equals button later
         operation = "+"
     }
 
@@ -112,13 +126,17 @@ class ViewController: UIViewController {
         operation = "/"
     }
 
+    //equal button action handler
     @IBAction func equalsClicked(sender: UIButton) {
+        //use a number formmatter so that if a number has .0 at the end it does not display it but if it has decimal values they are displayed up to 5 digits
         let formatter = NSNumberFormatter()
         formatter.minimumFractionDigits = 0
         formatter.maximumFractionDigits = 5
         
+        //set whatver is on the display as the second number
         num2 = (displayLabel.text! as NSString).doubleValue
         
+        //determine what operation we are performing based on the variable we set eariler
         switch (operation){
         case ("+"):
             displayLabel.text = formatter.stringFromNumber(num1 + num2)
@@ -131,6 +149,11 @@ class ViewController: UIViewController {
         default:
             displayLabel.text = "Error"
         }
+        
+        //reset both numbers and operation for the next equation
+        num1 = 0
+        num2 = 0
+        operation = ""
     }
 
 
