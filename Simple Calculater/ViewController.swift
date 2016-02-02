b648b8be0b430313af8ce1c4e95a004c657dda1a
student <student@students-mac.student.georgianc.on.ca>
2016-02-02 14:03:05 -0500

    added operation functionality

--- Simple Calculater/ViewController.swift
+++ Simple Calculater/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     var num1 = 0.0, num2 = 0.0
+    var operation = ""
 
     @IBAction func oneClicked(sender: UIButton) {
         if displayLabel.text?.characters.count < 6 {
@@ -72,11 +73,65 @@ class ViewController: UIViewController {
             displayLabel.text = displayLabel.text! + "0"
         }
     }
+    
+    @IBAction func decimalClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "."
+        }
+    }
+    
+    @IBAction func clearClicked(sender: UIButton) {
+        displayLabel.text = ""
+    }
 
+    @IBAction func AddClicked(sender: UIButton) {
+        num1 = (displayLabel.text! as NSString).doubleValue
+        displayLabel.text = ""
+        
+        operation = "+"
+    }
 
+    @IBAction func subtractClicked(sender: UIButton) {
+        num1 = (displayLabel.text! as NSString).doubleValue
+        displayLabel.text = ""
+        
+        operation = "-"
+    }
 
+    @IBAction func multplyClicked(sender: UIButton) {
+        num1 = (displayLabel.text! as NSString).doubleValue
+        displayLabel.text = ""
+        
+        operation = "*"
+    }
 
+    @IBAction func divideClicked(sender: UIButton) {
+        num1 = (displayLabel.text! as NSString).doubleValue
+        displayLabel.text = ""
+        
+        operation = "/"
+    }
 
+    @IBAction func equalsClicked(sender: UIButton) {
+        let formatter = NSNumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 5
+        
+        num2 = (displayLabel.text! as NSString).doubleValue
+        
+        switch (operation){
+        case ("+"):
+            displayLabel.text = formatter.stringFromNumber(num1 + num2)
+        case ("-"):
+            displayLabel.text = formatter.stringFromNumber(num1 - num2)
+        case ("*"):
+            displayLabel.text = formatter.stringFromNumber(num1 * num2)
+        case ("/"):
+            displayLabel.text = formatter.stringFromNumber(num1 / num2)
+        default:
+            displayLabel.text = "Error"
+        }
+    }
 
 
 
