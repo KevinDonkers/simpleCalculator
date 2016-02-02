1c400286ccc7d109fa788d4a2dd32a4494120d1a
student <student@students-mac.student.georgianc.on.ca>
2016-02-02 13:24:29 -0500

    got layout and number input working

--- Simple Calculater/ViewController.swift
+++ Simple Calculater/ViewController.swift
@@ -9,16 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var num1 = 0.0, num2 = 0.0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func oneClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "1"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func twoClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "2"
+        }
     }
+    
+    @IBAction func threeClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "3"
+        }
+    }
+
+    @IBAction func fourClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "4"
+        }
+    }
+
+    @IBAction func fiveClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "5"
+        }
+    }
+
+    @IBAction func sixClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "6"
+        }
+    }
+
+    @IBAction func sevenClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "7"
+        }
+    }
+
+    @IBAction func eightClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "8"
+        }
+    }
+
+    @IBAction func nineClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "9"
+        }
+    }
+
+    @IBAction func zeroClicked(sender: UIButton) {
+        if displayLabel.text?.characters.count < 6 {
+            displayLabel.text = displayLabel.text! + "0"
+        }
+    }
+
+
+
+
 
 
 }
